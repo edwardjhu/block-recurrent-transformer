5099da5aa66bbe437209eafdaaaa5b17379f5147
Edward Hu <edward@edwardjhu.com>
2022-08-29 00:05:36 +0000

    add better posterior model and critic model

--- train.py
+++ train.py
@@ -4,7 +4,7 @@ from numpy import broadcast
 from omegaconf import OmegaConf
 from torch.utils.data import RandomSampler, DataLoader
 from torch import nn
-from torch.nn.functional import cross_entropy
+from torch.nn.functional import cross_entropy, softplus
 import torch
 from tqdm import tqdm
 from transformers import AdamW, GPT2Tokenizer
@@ -156,6 +156,24 @@ class UpstreamTransformer(nn.Module):
             out = layer(out)
         return self.to_gaussian(out[:, -self.state_len:, :]), x
 
+class PosteriorMLP(nn.Module):
+    """As simple as I can make the model.
+    """
+
+    def __init__(self, input_dim, hidden_dim, output_dim, num_layers, double_output=True):
+        super().__init__()
+        self.input_layer = nn.Linear(input_dim, hidden_dim)
+        self.layers = nn.ModuleList([nn.Linear(hidden_dim, hidden_dim) for _ in range(num_layers)])
+        if double_output:
+            self.output_layer = nn.Linear(hidden_dim, 2*output_dim)
+        else:
+            self.output_layer = nn.Linear(hidden_dim, output_dim)
+    
+    def forward(self, x):
+        x = self.input_layer(x)
+        for layer in self.layers:
+            x = nn.functional.relu(layer(x))
+        return self.output_layer(x)
 
 def setup_tokenizer():
     tokenizer = GPT2Tokenizer.from_pretrained('gpt2-large')
@@ -185,6 +203,12 @@ def train(data, tokenizer_posterior, tokenizer_decoder, config):
     #upstream_model = UpstreamTransformer(config, len(tokenizer), config.d_latent)
     posterior_lstm.to(device)
 
+    posterior_mlp = PosteriorMLP(input_dim=config.d_model + config.d_latent,
+                                  hidden_dim=config.d_model,
+                                  output_dim=config.d_latent,
+                                  num_layers=1)
+    posterior_mlp.to(device)
+
     if os.path.exists(config.load_decoder):
         model = torch.load(config.load_decoder)
     else:
@@ -204,13 +228,23 @@ def train(data, tokenizer_posterior, tokenizer_decoder, config):
     else:
         #posterior_model = BiTransformer(config, len(tokenizer), config.d_model, d_latent=config.d_latent)
         posterior_model = RobertaModel.from_pretrained('roberta-base')
+        critic_mlp = PosteriorMLP(input_dim=config.d_latent+config.d_model,
+                                  hidden_dim=config.d_model,
+                                  output_dim=1,
+                                  num_layers=2,
+                                  double_output=False)
     posterior_model.to(device)
+    critic_mlp.to(device)
     #for p in posterior_model.parameters():
     #    p.requires_grad = False
 
-    opt = AdamW([{'params': chain(posterior_model.parameters(), model.parameters()), 'lr': config.lr, 'max_lr': config.lr}],
+
+    opt = AdamW([{'params': chain(posterior_model.parameters(),
+                                  model.parameters(),
+                                  posterior_mlp.parameters(),
+                                  critic_mlp.parameters()), 'lr': config.lr, 'max_lr': config.lr}],
                 betas=(config.adam_beta1, config.adam_beta2), weight_decay=0.0)
-    opt1 = AdamW([{'params': chain(upstream_model.parameters()), 'lr': 4*config.lr, 'max_lr': 4*config.lr}],
+    opt1 = AdamW([{'params': chain(upstream_model.parameters(), ), 'lr': 4*config.lr, 'max_lr': 4*config.lr}],
                 betas=(config.adam_beta1, config.adam_beta2), weight_decay=0.0)
     
     # cyclical annealing
@@ -227,7 +261,7 @@ def train(data, tokenizer_posterior, tokenizer_decoder, config):
                 v += step
                 i += 1
         return L 
-    annealing_sched = frange_cycle_linear(config.max_updates, start=0.0, stop=1.0, n_cycle=5)
+    annealing_sched = frange_cycle_linear(config.max_updates, start=0.0, stop=1.0, n_cycle=50)
 
     train_data = WikiDataset(data['train'])
     data_loader = DataLoader(train_data,  batch_size = config.batch_size, sampler = RandomSampler(train_data), pin_memory=True)
@@ -248,13 +282,14 @@ def train(data, tokenizer_posterior, tokenizer_decoder, config):
         '''
         # manually specify h0
         prev_state = [upstream_model.h0.repeat(article_batch.size(0), 1, 1)]
-        hidden = (prev_state[0].new_ones((config.lstm_layers, article_batch.size(0), config.d_model, )),
-                  prev_state[0].new_ones((config.lstm_layers, article_batch.size(0), config.d_model, )))
-        #hidden = prev_state[0].new_ones(article_batch.size(0), 0, config.d_latent)
+        hidden = (prev_state[0].new_zeros((config.lstm_layers, article_batch.size(0), config.d_model, )),
+                  prev_state[0].new_zeros((config.lstm_layers, article_batch.size(0), config.d_model, )))
+        #hidden = prev_state[0].new_zeros(article_batch.size(0), 0, config.d_latent)
         #hidden_posterior = \
-        #         (prev_state[0].new_ones((config.lstm_layers, article_batch.size(0), config.d_model, )),
-        #          prev_state[0].new_ones((config.lstm_layers, article_batch.size(0), config.d_model, )))
-        sampled_z = None
+        #         (prev_state[0].new_zeros((config.lstm_layers, article_batch.size(0), config.d_model, )),
+        #          prev_state[0].new_zeros((config.lstm_layers, article_batch.size(0), config.d_model, )))
+        #sampled_z = None
+        sampled_z = prev_state[0].new_zeros(article_batch.size(0), 1, config.d_latent)
         batch_rec_loss = 0.
         batch_prior_loss = 0.
         if global_step <= config.warmup:
@@ -275,8 +310,9 @@ def train(data, tokenizer_posterior, tokenizer_decoder, config):
                                                 text,
                                                 text.new_ones(text.size(0), 1)*tokenizer_posterior.eos_token_id], dim=-1)
             # run q(z|x)
-            pooled_posterior = posterior_model(bos_text_eos_posterior)['pooler_output']
+            encoded_xi = posterior_model(bos_text_eos_posterior)['pooler_output']
 
+            pooled_posterior = posterior_mlp(torch.cat([encoded_xi, sampled_z.squeeze(1)], dim=-1))
             # LSTM posterior
             #state_posterior, hidden_posterior = posterior_lstm(pooled_posterior.unsqueeze(1), hidden_posterior)
             #qz_mean = state_posterior[:, :, :config.d_latent]
@@ -285,26 +321,39 @@ def train(data, tokenizer_posterior, tokenizer_decoder, config):
             #eps = qz_mean.new(qz_mean.shape).normal_(0, 1)
             #torch.nn.init.trunc_normal_(eps)
 
-            if sampled_z is None:
-                pooled_posterior = model.hidden_to_z(pooled_posterior)
-                qz_mean = pooled_posterior.unsqueeze(1)[:, :, :config.d_latent]
-                qz_logvar = pooled_posterior.unsqueeze(1)[:, :, config.d_latent:]
-                eps = qz_mean.new(qz_mean.shape).normal_(0, 1)
-                torch.nn.init.trunc_normal_(eps)
-                qz_mean_true = qz_mean
-                sampled_z = qz_mean_true + (0.5 * qz_logvar).exp() * eps
+            #if sampled_z is None:
+            #pooled_posterior = model.hidden_to_z(pooled_posterior)
+            qz_mean = pooled_posterior.unsqueeze(1)[:, :, :config.d_latent]
+            qz_logvar = pooled_posterior.unsqueeze(1)[:, :, config.d_latent:]
+            #qz_var = softplus(pooled_posterior.unsqueeze(1)[:, :, config.d_latent:])
+            eps = qz_mean.new(qz_mean.shape).normal_(0, 1)
+            torch.nn.init.trunc_normal_(eps)
+            qz_mean_true = qz_mean
+            sampled_z = qz_mean_true + (0.5 * qz_logvar).exp() * eps
+            #sampled_z = qz_mean_true + qz_var**0.5 * eps
+            '''
             else:
                 pooled_posterior = model.hidden_to_z(pooled_posterior)
                 qz_mean = pooled_posterior.unsqueeze(1)[:, :, :config.d_latent]
-                qz_logvar = pooled_posterior.unsqueeze(1)[:, :, config.d_latent:]
+                #qz_logvar = pooled_posterior.unsqueeze(1)[:, :, config.d_latent:]
+                qz_var = softplus(pooled_posterior.unsqueeze(1)[:, :, config.d_latent:])
                 eps = qz_mean.new(qz_mean.shape).normal_(0, 1)
                 torch.nn.init.trunc_normal_(eps)
                 qz_mean_true = sampled_z * 0.5 + qz_mean
-                sampled_z = qz_mean_true + (0.5 * qz_logvar).exp() * eps
+                #sampled_z = qz_mean_true + (0.5 * qz_logvar).exp() * eps
+                sampled_z = qz_mean_true + qz_var**0.5 * eps
+            '''
             preds = model(inputs_embeds=torch.cat([model.z_to_hidden(sampled_z),
                                 model.get_input_embeddings()(inputs)], dim=1))['logits'][:,sampled_z.size(1):,:]
             
-            state, hidden = upstream_model(sampled_z.detach(), hidden)
+            state, hidden = upstream_model(sampled_z, hidden)
+
+            # critic model
+            sampled_z = sampled_z.squeeze(1)
+            critic_loss = -torch.cat([critic_mlp(torch.cat([sampled_z, encoded_xi.roll(i, dims=0)], dim=-1)) \
+                                        for i in range(sampled_z.size(0))], dim=-1).log_softmax(dim=-1)[:, 0]
+            critic_loss = critic_loss.mean()
+
 
             # reconstruction loss
             rec_loss = cross_entropy(preds[inputs_mask], targets[inputs_mask])
@@ -312,11 +361,13 @@ def train(data, tokenizer_posterior, tokenizer_decoder, config):
             if config.state_len > 0:
                 pz_mean = prev_state[-1][:, :, :config.d_latent]
                 pz_logvar = prev_state[-1][:, :, config.d_latent:]
-                prior_loss = 0.5 * ((pz_logvar-qz_logvar.detach()) + (qz_logvar.detach().exp()+(qz_mean_true.detach() - pz_mean)**2)/pz_logvar.exp() - 1).sum(-1).mean()
+                #pz_var = softplus(prev_state[-1][:, :, config.d_latent:])
+                prior_loss = 0.5 * ((pz_logvar-qz_logvar) + (qz_logvar.exp()+(qz_mean_true - pz_mean)**2)/pz_logvar.exp() - 1).sum(-1).mean()
+                #prior_loss = 0.5 * ((pz_var/qz_var.detach()).log() + (qz_var.detach()+(qz_mean_true.detach() - pz_mean)**2)/pz_var - 1).sum(-1).mean()
                 # variants
                 #prior_loss = ((pz_logvar-qz_logvar.detach())**2+(qz_mean_true.detach() - pz_mean)**2).sum(-1).mean()
                 #prior_loss = 0.5 * ((pz_logvar-qz_logvar) + (qz_logvar.exp()+(qz_mean_true - pz_mean)**2)/pz_logvar.exp() - 1).sum(-1).mean()
-                #prior_reg = 0.001*(0.5 * ((-qz_logvar) + (qz_logvar.exp()+(qz_mean)**2) - 1).sum(-1).mean())
+                prior_reg = 0.1*(0.5 * ((-qz_logvar) + (qz_logvar.exp()+(qz_mean)**2) - 1).sum(-1).mean())
                 #prior_reg = 0.005*(0.5 * ((-qz_logvar) + (qz_logvar.exp()+(qz_mean)**2) - 1).sum(-1).mean())
                 #prior_loss = ((qz_mean.detach() - pz_mean)**2).sum(-1).mean()
                 #prior_loss = ((pz_logvar-qz_logvar.detach())**2 + (qz_mean.detach() - pz_mean)**2).sum(-1).mean()
@@ -326,21 +377,27 @@ def train(data, tokenizer_posterior, tokenizer_decoder, config):
             #if rec_loss > 1:
             #    batch_rec_loss += rec_loss #+prior_reg
             #else:
-            batch_rec_loss += rec_loss
+
+            # critic + recon
+            #batch_rec_loss += rec_loss + critic_loss #+ prior_reg
+            # critic only
+            batch_rec_loss += critic_loss #+ prior_reg
             batch_prior_loss += prior_loss #+prior_reg
             
         train_loss += (batch_rec_loss+batch_prior_loss).item() / i
         recon_loss += batch_rec_loss.item() / i
         
-        #(0.1*annealing_sched[global_step]*batch_prior_loss + batch_rec_loss).backward()
-        (batch_prior_loss + batch_rec_loss).backward()
-        if global_recon_step < config.max_recon_updates:
-            torch.nn.utils.clip_grad_norm_(chain(model.parameters(), posterior_model.parameters()), config.gclip)
-            opt.step()
-            global_recon_step += 1
-        else:
-            torch.nn.utils.clip_grad_norm_(upstream_model.parameters(), config.gclip)
-            opt1.step()
+        # set the coe below to 0 to disable KL
+        (0.0005*annealing_sched[global_step]*batch_prior_loss + batch_rec_loss).backward()
+        #(0.01 * batch_prior_loss + batch_rec_loss).backward()
+        #if global_recon_step < config.max_recon_updates:
+        #if (global_step // 500) % 2 == 0:
+        #torch.nn.utils.clip_grad_norm_(chain(posterior_mlp.parameters(), model.parameters(), posterior_model.parameters()), config.gclip)
+        opt.step()
+        global_recon_step += 1
+        #else:
+        #torch.nn.utils.clip_grad_norm_(chain(upstream_model.parameters()), config.gclip)
+        opt1.step()
         global_step += 1
         log_freq = 100
         if batch_id % log_freq == 0:
@@ -381,14 +438,14 @@ def valid(model, posterior_model, upstream_model, posterior_lstm,
         '''
         # manually specify h0
         prev_state = upstream_model.h0.repeat(article_batch.size(0), 1, 1)
-        hidden = (prev_state[0].new_ones((config.lstm_layers, article_batch.size(0), config.d_model, )),
-                  prev_state[0].new_ones((config.lstm_layers, article_batch.size(0), config.d_model, )))
-        #hidden = prev_state[0].new_ones(article_batch.size(0), 0, config.d_latent)
+        hidden = (prev_state[0].new_zeros((config.lstm_layers, article_batch.size(0), config.d_model, )),
+                  prev_state[0].new_zeros((config.lstm_layers, article_batch.size(0), config.d_model, )))
+        #hidden = prev_state[0].new_zeros(article_batch.size(0), 0, config.d_latent)
         hidden_posterior = \
-                 (prev_state[0].new_ones((config.lstm_layers, article_batch.size(0), config.d_model, )),
-                  prev_state[0].new_ones((config.lstm_layers, article_batch.size(0), config.d_model, )))
+                 (prev_state[0].new_zeros((config.lstm_layers, article_batch.size(0), config.d_model, )),
+                  prev_state[0].new_zeros((config.lstm_layers, article_batch.size(0), config.d_model, )))
         sampled_z = None
-        #hidden = prev_state[0].new_ones((article_batch.size(0), 0, config.d_latent))
+        #hidden = prev_state[0].new_zeros((article_batch.size(0), 0, config.d_latent))
         for i, text in enumerate(long_sequence_splitter(article_batch, config.window_len)):
             # add eos token so the low-level model knows when to stop
             bos_text_decoder = torch.cat([text.new_ones(text.size(0), 1)*tokenizer_decoder.bos_token_id, text], dim=-1)
@@ -449,10 +506,10 @@ def generate(model, upstream_model, tokenizer, config, mode='argmax'):
     upstream_model.eval()
 
     prev_state = upstream_model.h0.repeat(1, 1, 1)
-    hidden = (prev_state[0].new_ones((config.lstm_layers, 1, config.d_model, )),
-              prev_state[0].new_ones((config.lstm_layers, 1, config.d_model, )))
-    #hidden = prev_state[0].new_ones(prev_state[0].size(0), 0, config.d_latent)
-    #hidden = prev_state[0].new_ones((1, 0, config.d_latent))
+    hidden = (prev_state[0].new_zeros((config.lstm_layers, 1, config.d_model, )),
+              prev_state[0].new_zeros((config.lstm_layers, 1, config.d_model, )))
+    #hidden = prev_state[0].new_zeros(prev_state[0].size(0), 0, config.d_latent)
+    #hidden = prev_state[0].new_zeros((1, 0, config.d_latent))
     decoded = []
     for i in range(10):
         pz_mean = prev_state[:, :, :config.d_latent]
@@ -464,7 +521,7 @@ def generate(model, upstream_model, tokenizer, config, mode='argmax'):
         bos_text = (pz_mean.new_ones(pz_mean.size(0), 1)*tokenizer.bos_token_id).long().cuda()
         for _ in range(config.window_len):
             preds = model(inputs_embeds=torch.cat([model.z_to_hidden(sampled_z),
-                          model.get_input_embeddings()(bos_text)], dim=1))['logits'][:,1:,:]
+                        model.get_input_embeddings()(bos_text)], dim=1))['logits'][:,sampled_z.size(1):,:]
             if mode == 'sample':
                 bos_text = torch.cat([bos_text,
                                     preds[:, -1, :].softmax(dim=-1).multinomial(num_samples=1)], dim=-1).cuda()
